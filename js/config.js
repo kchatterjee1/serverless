deae98580c14efc0f221821b7a8da4111fe17aa8
kchatterjee1 <kchatterjee1@users.noreply.github.com>
2019-10-07 13:04:38 -0400

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
         userPoolId: 'us-east-1_A4JrymRSR', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '4446t2arj1usf2dbgvi1464cr7', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolClientId: '6c8rh2krb55efucpf7bia4ihnb', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
